77dbf95ad05db7da9e6e21e44a1b1f8005c844bd
tatamiya <tamyuji1002@gmail.com>
2021-04-02 20:31:07 +0900

    3.3 Functions

---
+++ chapter3/functions/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    println!("Hello, world!");
+    another_function(5, 6);
+
+    let x = 5;
+    let y = {
+        let x = 3;
+        x + 1 // whthout ;
+    };
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+
+    let x = five();
+    println!("The value of x is: {}", x);
+
+    let x = plus_one(5);
+    println!("The value of x is: {}", x);
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5 // without ;
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1 // without ;
+}

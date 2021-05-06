53d4e9434c0c46612af804f4bb59d1930e0eeb87
tatamiya <tamyuji1002@gmail.com>
2021-05-06 19:40:02 +0900

    Chapter 6: The match Control Flow Operator

---
+++ chapter6_enums_and_pattern_matching/mach_control_flow_operator/src/main.rs
@@ -0,0 +1,44 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // etc...
+}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    println!("{}", value_in_cents(Coin::Penny));
+    println!("{}", value_in_cents(Coin::Quarter(UsState::Alabama)));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("{:?}", six);
+    let none = plus_one(None);
+    println!("{:?}", none);
+}

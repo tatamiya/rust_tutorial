9a7a0f3628230575c11261255150605273f2b156
tatamiya <tamyuji1002@gmail.com>
2021-05-07 13:31:59 +0900

    Chapter7: Paths for Referring to an Item in the Module Tree

--- chapter7_packages_crates_modules/restaurant/src/lib.rs
+++ chapter7_packages_crates_modules/restaurant/src/lib.rs
@@ -1,15 +1,54 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
+}
 
-    mod serving {
-        fn take_order() {}
+fn serve_order() {}
 
-        fn serve_order() {}
+mod back_of_house {
+    fn cook_order() {}
 
-        fn take_payment() {}
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
     }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+pub fn eat_at_resutaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Weat");
+    // Error!: meal.seasonal_fruit = String::from("blueberries");
+    println!("I'd like {} toast please", meal.toast);
+
+    // variants of public enum are public in default.
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }

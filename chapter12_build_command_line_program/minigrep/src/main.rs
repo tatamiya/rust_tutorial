f3831679c26aa7a94d45a07387b21290f544ef3f
tatamiya <tamyuji1002@gmail.com>
2021-05-19 20:55:01 +0900

    Chapter 12: Refactoring (Extracting Logic from main)

--- chapter12_build_command_line_program/minigrep/src/main.rs
+++ chapter12_build_command_line_program/minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -13,10 +14,11 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
 
-    println!("With text:\n{}", contents);
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -34,4 +36,12 @@ impl Config {
 
         Ok(Config { query, filename })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
 }

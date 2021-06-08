766e5bfc83e2f526b3902a14b7aa8777a0455d79
tatamiya <tamyuji1002@gmail.com>
2021-06-08 20:35:43 +0900

    Ch17: Implementing an Object-Oriented Design Pattern (upto Listing 21)

---
+++ chapter17_object_oriented_programming_features/another_blog/src/main.rs
@@ -0,0 +1,12 @@
+use another_blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+}

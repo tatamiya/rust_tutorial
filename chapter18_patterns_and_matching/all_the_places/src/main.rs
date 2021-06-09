b0adc08858b1e529279ca4e17176d114b9c07235
tatamiya <tamyuji1002@gmail.com>
2021-06-09 20:14:50 +0900

    Ch18: All the Places Patterns Can Be Used

---
+++ chapter18_patterns_and_matching/all_the_places/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    // while let Conditional Loops
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+
+    // for Loops
+    let v = vec!['a', 'b', 'c'];
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+
+    // Function Parameters
+    fn print_coordinates(&(x, y): &(i32, i32)) {
+        println!("Current location: ({}, {})", x, y);
+    }
+
+    let point = (3, 5);
+    print_coordinates(&point);
+}

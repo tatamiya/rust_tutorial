516068ad41e53cd4d0efed5632b620ca26b9f4e7
tatamiya <tamyuji1002@gmail.com>
2021-06-11 20:59:36 +0900

    Ch18: Pattern Syntax (upto Listing 18)

--- chapter18_patterns_and_matching/pattern_syntax/src/main.rs
+++ chapter18_patterns_and_matching/pattern_syntax/src/main.rs
@@ -59,4 +59,68 @@ fn main() {
         Point { x: 0, y } => println!("On the y axis at {}", y),
         Point { x, y } => println!("On neither axsi: ({}, {})", x, y),
     }
+
+    // Destructuring Enums
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+    let msg = Message::ChangeColor(0, 150, 255);
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {} and in the y direction {}", x, y)
+        }
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        }
+    }
+
+    // Destructuring Nested Structs and Enums
+    enum Color {
+        Rgb(i32, i32, i32),
+        Hsv(i32, i32, i32),
+    }
+    enum Message2 {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(Color),
+    }
+
+    let msg = Message2::ChangeColor(Color::Hsv(0, 160, 255));
+    match msg {
+        Message2::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        }
+        Message2::ChangeColor(Color::Hsv(h, s, v)) => println!(
+            "Change the color to hue {}, saturation {}, and value {}",
+            h, s, v
+        ),
+        _ => (),
+    }
+
+    // Ignoring an Entire Value with _
+    fn foo(_: i32, y: i32) {
+        println!("This code only uses the y parameter: {}", y);
+    }
+    foo(3, 4);
+
+    // Ignoring Parts of a Value with a Nested _
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+    println!("setting is {:?}", setting_value);
 }

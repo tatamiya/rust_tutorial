3810c384c8b1ebc7ac4c64a368af4b0da325c1bc
tatamiya <tamyuji1002@gmail.com>
2021-06-13 19:33:47 +0900

    Ch19: Unsafe Rust (upto Listing 6)

---
+++ chapter19_advanced_features/unsafe_rust/src/main.rs
@@ -0,0 +1,34 @@
+use std::slice;
+
+fn main() {
+    // Dereferencing a Raw Pointer
+    let mut num = 5;
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+
+    // Calling an Unsafe Function or Method
+
+    // Creating a Safe Abstraction over Unsafe Code
+    fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+        let len = slice.len();
+        let ptr = slice.as_mut_ptr();
+
+        assert!(mid <= len);
+        unsafe {
+            (
+                slice::from_raw_parts_mut(ptr, mid),
+                slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+            )
+        }
+    }
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let r = &mut v[..];
+    let (a, b) = r.split_at_mut(3);
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+}

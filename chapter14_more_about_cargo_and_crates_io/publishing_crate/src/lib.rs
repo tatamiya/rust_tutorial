e352c29b9ee901f3ded9c04f03885bb3fed9de95
tatamiya <tamyuji1002@gmail.com>
2021-06-19 19:06:57 +0900

    Ch14: Publishing a Crate to Crates.io(Making Useful Documentation Comments)

---
+++ chapter14_more_about_cargo_and_crates_io/publishing_crate/src/lib.rs
@@ -0,0 +1,19 @@
+//! # Publishing Crate
+//!
+//! `publishing_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = publishing_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}

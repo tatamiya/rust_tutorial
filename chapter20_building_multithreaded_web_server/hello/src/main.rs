326108c90a4ea93a97ed8757c68ab7e8d2fe7a7d
tatamiya <tamyuji1002@gmail.com>
2021-06-22 21:13:20 +0900

    Ch20: Building a Single-Threaded Web Server (upto Listing 5)

--- chapter20_building_multithreaded_web_server/hello/src/main.rs
+++ chapter20_building_multithreaded_web_server/hello/src/main.rs
@@ -1,4 +1,7 @@
+use std::fs;
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,6 +9,22 @@ fn main() {
     for stream in listner.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let contents = fs::read_to_string("hello.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }

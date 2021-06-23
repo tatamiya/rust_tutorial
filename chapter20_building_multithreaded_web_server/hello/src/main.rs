74fbb4969cae56a2a49f30e2f7f8cf0ab47f5017
tatamiya <tamyuji1002@gmail.com>
2021-06-23 21:05:13 +0900

    Ch20: Turning Our Single-Threaded Server into a Multithreaded Server (Building the ThreadPool Struct Using Compiler Driven Development)

--- chapter20_building_multithreaded_web_server/hello/src/main.rs
+++ chapter20_building_multithreaded_web_server/hello/src/main.rs
@@ -5,13 +5,18 @@ use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
 
+use hello::ThreadPool;
+
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listner.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
